0f2caf25f8f6ff7af9a26b4ecb4991212f9d3ce2
Aaron <aaron_khiev@uri.edu>
2024-04-20 20:48:07 -0400

    Assignment 12 Edit

---
+++ HTML/GM-Page/insert_camp.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+
+<?php
+	// Include the session script
+	require '../../includes/database-connection.php';
+?>
+
+<?php 
+    $campTitle=$_POST['campTitle'];
+    $theme=$_POST['theme'];
+    $session=$_POST['session'];
+
+    $sql='INSERT INTO campaign (campTitle, theme, session)
+    VALUES (:campTitle, :theme, :session);';
+
+    $newc = ['campTitle'] = $campTitle;
+    $newc = ['theme'] = $theme;
+    $newc = ['session'] = $session;
+
+    $statement = $pdo->prepare($sql);
+    $statement->execute($newc);
+
+    pdo($pdo, $sql, $arguments);
+    $new_id = $pdo->lastInsertId() - 1;    
+
+    // Retrieve ALL character info from the characters table. Campaign info will be displayed on this page.
+	$sql = "SELECT * 
+    FROM campaign";
+
+    // Execute the SQL query using the pdo function and fetch the result
+    $camp = pdo($pdo, $sql)->fetchAll();
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Fantasy Table Top Compendium Homepage</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../../CSS/myStyle.css">
+    <link rel="stylesheet" type="text/css" href="../../CSS/navigation.css">
+    <link rel="stylesheet" type="text/css" href="../../CSS/character.css">
+    <link rel="stylesheet" type="text/css" href="../../CSS/npc.css">
+    <?php include '../../character.php'; ?>
+</head>
+
+<body>
+    <div id="content">
+        <div id="logo">
+            <img src="../../Images/LogoCrop.png" alt="Site Logo" style="width: 100%;" height="240">
+        </div>
+        <hr />
+
+        <h1 id="title">The Table Top Compendium</h1>
+        <hr />
+
+        <div class="topnav">
+            <a href="../index.html">Home</a>
+            <a href="../DD-Page/index.html">The Dungeon Delver's Guide</a>
+            <a href="../Generators-Page/index.html">Generators</a>
+            <a class="active" href="char.php">Game Master's World Anvil</a>
+            <a href="../about.html">About</a>
+        </div>
+        <hr />
+
+        <h2>Campaign Created</h2>
+        <ul>
+            <li><strong>Campaign Title: </strong><?= $camp[$new_id]['campTitle'] ?></li>
+            <li><strong>Theme: </strong><?= $camp[$new_id]['theme'] ?></li>
+            <li><strong>Sessions Played: </strong><?= $camp[$new_id]['session'] ?></li>
+        </ul>
+        <hr />
+        
+        <h2>External Resources</h2>
+            <p>
+                Do you still feel unprepared? Need DM resources not for players? No worries! Check out this site for more~! <a id="comp" href="https://www.dnd-compendium.com/">The DND Compendium.</a>
+            </p>
+        <hr />
+
+        <h2>Learn More Here</h2>
+        <div class="subnav">
+            <a href="skills.html"> Skills, Skill Checks, and Success</a>
+            <a href="action.html"> Taking Initiative</a>
+            <a href="spell.html"> Spellcasting 101</a>
+        </div>
+
+        <br />
+        <div id="Contact">
+            <hr />
+            <h3>Contact Us!</h3>
+            <address>
+                <p>
+                    Aaron Khiev:
+                    <a href="mailto:aaron_khiev@uri.edu">aaron_khiev@uri.edu</a>
+                </p>
+            </address>
+        </div>
+    </div>
+</body>
+
+</html>
